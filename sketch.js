76f2bed30f8a63da6b5a69aa1d93cbcbb531228c
Justin <heartleyjm@gmail.com>
2024-05-06 14:31:43 -0400

    STYLE: format document

--- sketch.js
+++ sketch.js
@@ -1,73 +1,73 @@
-let audio, amp, fft
+let audio, amp, fft;
 
-let isPressed = false
+let isPressed = false;
 // const bins = 64
 // let binWidth 
 // let peakDetect
 
-let myShader 
+let myShader;
 
-let angle = 0.0
-let jitter = 0.0
+let angle = 0.0;
+let jitter = 0.0;
 
 function preload() {
-  audio = loadSound('audio/Are you gonna dance or what.mp3')
-  myShader = loadShader('shader/vertex.vert', 'shader/fragment.frag')
-  frameRate(60)
+  audio = loadSound('audio/Are you gonna dance or what.mp3');
+  myShader = loadShader('shader/vertex.vert', 'shader/fragment.frag');
+  frameRate(60);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL)
+  createCanvas(windowWidth, windowHeight, WEBGL);
 
-  shader(myShader)
+  shader(myShader);
   // getAudioContext().suspend()
   // audio.setVolume(0.1)
-  
-  userStartAudio()
-  
+
+  userStartAudio();
+
   // fill(255, 255, 255)
 
-  amp = new p5.Amplitude()
-  fft = new p5.FFT()
+  amp = new p5.Amplitude();
+  fft = new p5.FFT();
 
   // peakDetect = new p5.PeakDetect()
   // peakDetect.onPeak(peakDetected)
 }
 
 function draw() {
-  background(200, 200, 234, 200) 
+  background(200, 200, 234, 200);
   // background(0)
-  
-  drawingContext.filter = 'blur(px)'
 
-  console.log(audio.currentTime())
+  drawingContext.filter = 'blur(px)';
+
+  console.log(audio.currentTime());
 
   // stroke(255)
 
-  fft.analyze()
+  fft.analyze();
 
-  const volume = amp.getLevel()
-  let freq = fft.getCentroid()
-  
-  freq *= 0.001
-  
+  const volume = amp.getLevel();
+  let freq = fft.getCentroid();
+
+  freq *= 0.001;
+
   if (second() % 2 == 0) {
-    jitter = random(0, 0.1)
-    jitter += jitter
+    jitter = random(0, 0.1);
+    jitter += jitter;
   }
 
-  angle = angle + jitter
+  angle = angle + jitter;
 
-  rotateX(sin(freq) + angle * 0.1)
-  rotateY(cos(volume) + angle * 0.1)
+  rotateX(sin(freq) + angle * 0.1);
+  rotateY(cos(volume) + angle * 0.1);
 
-  
 
-  const mapF = map(freq, 0, 1, 0, 20)
-  const mapV = map(volume, 0, 0.2, 0, 0.5)
+
+  const mapF = map(freq, 0, 1, 0, 20);
+  const mapV = map(volume, 0, 0.2, 0, 0.5);
   // translate(0, height / 2)
   // rect(0, 0, mapW, mapW)
-  console.log()
+  console.log();
   // const waveform = audio.getPeaks()
 
   // for(let i = 0; i < waveform.length; i++){
@@ -81,24 +81,24 @@ function draw() {
   //   const y = map(waveform[i], -1, 1, 0, height)
   //   point(x, y) 
   // } 
-  myShader.setUniform('uTime', frameCount)
+  myShader.setUniform('uTime', frameCount);
 
-  myShader.setUniform('uFreq', mapF)
-  myShader.setUniform('uAmp', mapV)
+  myShader.setUniform('uFreq', mapF);
+  myShader.setUniform('uAmp', mapV);
 
-  
-  sphere(200, 400, 400)
-  
+
+  sphere(200, 400, 400);
+
 }
 
 function mousePressed() {
   if (isPressed) {
-    audio.pause()
-    isPressed = false
+    audio.pause();
+    isPressed = false;
   }
   else {
-    isPressed = true
-    audio.loop()
+    isPressed = true;
+    audio.loop();
   }
 }
 
